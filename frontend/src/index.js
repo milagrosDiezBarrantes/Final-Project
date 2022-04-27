4e7600b2de5dcf3d46ad1756c4730f5f9f98002b
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-04-26 21:37:41 -0300

    Login perra

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-import { Provider } from 'react-redux';
-import { store } from './Redux/Store/store'
-
+import { Provider } from "react-redux";
+import { store } from "./Redux/Store/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<React.StrictMode>
+			<Auth0Provider
+				domain="dev-8dlcilpo.us.auth0.com"
+				clientId="dNHRYGKTvuFiLFozoy5y7vUawIXvbpZc"
+				redirectUri={window.location.origin}
+			>
+				<App />
+			</Auth0Provider>
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
