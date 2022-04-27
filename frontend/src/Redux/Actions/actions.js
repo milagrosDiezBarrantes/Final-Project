c2ab373485c85d8e505ea065888333e09594acf2
chenaoh <milagrosdiezbarrantescm@gmail.com>
2022-04-27 15:31:18 -0300

    rama eliminar

--- frontend/src/Redux/Actions/actions.js
+++ frontend/src/Redux/Actions/actions.js
@@ -1,1 +1,12 @@
-import axios from 'axios'; 
+import axios from 'axios';
+
+export function getCharacters(){
+    return async function(dispatch){
+        dispatch({type:'Loading'})
+        let json = await axios('/characters')
+        return dispatch({
+            type: 'getCharacters',
+            payload: json.data
+        })
+    }
+}
--- frontend/src/Redux/Reducers/reducers.js
+++ frontend/src/Redux/Reducers/reducers.js
@@ -5,10 +5,40 @@ const initialState = {
     copyComics: [],
 }
 
-function rootReducer(state = initialState, action) {
-    // eslint-disable-next-line default-case
-    switch (action.type) {
-
+function rootReducer(state = initialState, {type, payload}) {
+    switch (type) {
+        case 'getCharacters':
+            return{
+                   ...state, 
+                   Characters: payload, 
+                   copyCharacters: payload, 
+                   loading: false
+                };
+        case 'getTypes':
+            return{
+                   ...state, 
+                   types: payload
+                }   
+        case 'getCharactersByName':
+            return{
+                ...state, 
+                copyCharacters: payload, 
+                loading: false
+            }        
+         case 'getCharactersById':
+               return {
+                   ...state, 
+                }
+         case 'filterByTypes':
+                const allTypes = state.allCharacters
+                const typesFilter = payload === 'all' ? allTypes : allTypes.filter(character => {
+                for(let type of character.type){
+                if(payload === type){
+                    return character
+                    } 
+                }
+                });
+                return{...state, copyCharacters: typesFilter}            
 
         default:
             return { ...state };
