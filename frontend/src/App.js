225fa5803dcda2e04921971bde5a4339f20d9c27
DESKTOP-30KTMEV\Equipo <japr0990@gmail.com>
2022-04-28 15:57:18 -0300

    se doluciona un problema de typeo y ruta default

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<div>ruta default, cliente levantado</div>}/>
           <Route path='/login' element={<Login />} />
           <Route path='/LandingPage' element={<LandingPage />} />
           <Route path='/homeCharacter' element={<HomeCharacter />} />
