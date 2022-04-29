040d049c9f0ece3fc135abac8920b92825b657e5
Alfredo Egremy <65094180+Egremy-quark@users.noreply.github.com>
2022-04-29 01:32:23 -0500

    Paginado en comics

--- frontend/src/Components/HomeComponent/HomeComic/HomeComics.jsx
+++ frontend/src/Components/HomeComponent/HomeComic/HomeComics.jsx
@@ -1,30 +1,81 @@
-import React from 'react'
-import SearchBarComics from '../../SearchBar/SearchBarComics/SearchBarComics'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import SearchBarComics from '../../SearchBar/SearchBarComics/SearchBarComics'
+import ReactPaginate from "react-paginate"
 
 import NavBar from '../../Navbar/Navbar.jsx';
 import ComicCard from '../../Cards/ComicCard/ComicCard';
 
 import { getAllComics } from '../../../Redux/Actions/actions'
+
 const HomeComics = () => {
 
+    const dispatch = useDispatch()
     const allComics = useSelector(state => state.ComicsReducer.copyComics)
 
-    const dispatch = useDispatch()
+    //===========Paginado===============//
 
+
+    const PER_PAGE = 8;
+
+    const [currentPage, setCurrentPage] = useState(0);
+    const [data, setData] = useState([]);
+
+    //Traer datos
     useEffect(() => {
         dispatch(getAllComics());
     }, [dispatch])
+
+    useEffect(() => {
+        setData(allComics)
+    }, [allComics])
+
+    const handlePageClick = ({ selected: selectedPage }) => {
+        setCurrentPage(selectedPage)
+    }
+    const offset = currentPage * PER_PAGE;
+
+    //Aquí mapeamos los datos del paginado
+    const currentPageData = data ?
+        data.slice(offset, offset + PER_PAGE)
+            .map(({ id, title, img }) => {
+                return (
+                    <div key={id} className='card'>
+                        <ComicCard
+                            id={id}
+                            title={title}
+                            image={img}
+                        />
+
+                    </div>
+                )
+
+            })
+        : <p>No hay nada </p>
+
+    const pageCount = Math.ceil(data.length / PER_PAGE);
 
     return (
         <div>
             <NavBar />
             <SearchBarComics />
+            <div className='paginas'>
+                <ReactPaginate
+                    previousLabel={'<- Previous'}
+                    nextLabel={'Next ->'}
+                    pageCount={pageCount}
+                    onPageChange={handlePageClick}
+                    containerClassName={'pagination'}
+                    previousLinkClassName={'pagination_link'}
+                    nextLinkClassName={'pagination_link'}
+                    disabledClassName={'pagination_link--disabled'}
+                    activeClassName={'pagination_link--active'}
+                />
+            </div>
 
             {/* state.map((e)=><div> <Card name=${}  </div>) */}
 
-            {allComics.length === 0 ? <h1>Loading...</h1> :
+            {/* {allComics.length === 0 ? <h1>Loading...</h1> :
 
                 allComics.map((comic) => (
                     <div key={comic.id}>
@@ -34,7 +85,12 @@ const HomeComics = () => {
                             title={comic.title}
                             image={comic.img} />
                     </div>)
-                )}
+                )} */}
+            <div className='all_games'>
+                {currentPageData}
+            </div>
+
+
         </div>
     )
 
