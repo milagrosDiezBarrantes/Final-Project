74a7fdcc5547434692100d1f21c615edd39293de
DESKTOP-30KTMEV\Equipo <japr0990@gmail.com>
2022-04-29 19:51:21 -0300

    loading con iconos

---
+++ frontend/src/Components/Loading/Loading.jsx
@@ -0,0 +1,41 @@
+import React,{useEffect, useState} from "react";
+import "./Loading.css"
+import spider from "../../Img/Loading/spaider.gif"
+import iron from "../../Img/Loading/ironman2.webp"
+import varios from "../../Img/Loading/varios_icons.gif"
+const Loading = ()=>{
+    const [img,setImg] = useState([varios,iron,])
+    var [num,setNum] = useState(0)
+
+    const cambio = ()=>{
+        setTimeout(()=>{
+            if(num = img.length-1){
+                setNum(0)
+                console.log(num)
+            }else if(num < img.length-1){
+                setNum(...num+1)
+                console.log(num)
+            }  
+            
+        },1000)
+        
+    }
+    useEffect(()=>{
+     cambio()   
+    },[num])
+    return (
+        
+            <div  className="Contenedor_de_carga" >
+                <div className="Girar">
+                    <div className="CambiarColor">
+                            {<img className="img" src={varios}/>}
+                    </div>
+                </div>
+            </div>
+        
+    )
+}
+
+
+
+export default Loading
