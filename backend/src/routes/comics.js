9d1765df803448af6a79cd1eff4015e45c0f7a16
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-01 15:15:33 -0300

    nueva ruta comics*-series

--- backend/src/routes/comics.js
+++ backend/src/routes/comics.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 
 const router = Router();
 
-const { getComics, getById  } = require('../controllers/comics.js');
+const { getComics, getById, getSerieById } = require('../controllers/comics.js');
 
 
 router.get('/', getComics);
 router.get('/:id', getById);
+router.get('/serie/:id', getSerieById);
 
 module.exports = router;
