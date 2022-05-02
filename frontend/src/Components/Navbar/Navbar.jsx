dd7e7d501778eb69f26b61301158ea16af79a2ef
chenaoh <milagrosdiezbarrantescm@gmail.com>
2022-05-02 12:19:13 -0300

    navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import DrawerComp from "../Navbar/Drawer";
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import {
   AppBar,
   Button,
