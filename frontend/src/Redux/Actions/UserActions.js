5aa973d71f19e86771f07a82e22b0820e27b0efa
DESKTOP-30KTMEV\Equipo <japr0990@gmail.com>
2022-05-03 13:58:28 -0300

    se actualiza el redux para admin y perfiles

---
+++ frontend/src/Redux/Actions/UserActions.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+//========== Export de las ActionsType ==========// ej: export const ACTIONTYPE = "ACTIONTYPE"
+
+
+//========== Export de las Actions ==========//
--- frontend/src/Redux/Reducers/index.js
+++ frontend/src/Redux/Reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import CharactersReducer from './CharacterReducer'
 import ComicsReducer from './ComicReducer'
-
+import AdminReducer from './AdminReducer'
+import UserReducer from './UserReducer'
 const rootReducer = combineReducers({
     CharactersReducer,
-    ComicsReducer
+    ComicsReducer,
+    AdminReducer,
+    UserReducer
 })
 
 
