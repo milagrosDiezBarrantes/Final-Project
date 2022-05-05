8fa1d09e43dbb5bd5aa4acc7609cd65432089b68
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-05 08:20:03 -0300

    ruta post comic creada

--- backend/src/routes/comics.js
+++ backend/src/routes/comics.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 
 const router = Router();
 
-const { getComics, getById, getSerieById } = require('../controllers/comics.js');
+const { getComics, getById, getSerieById, createComic } = require('../controllers/comics.js');
 
 
 router.get('/', getComics);
 router.get('/:id', getById);
 router.get('/serie/:id', getSerieById);
-
+router.post('/', createComic);
 module.exports = router;
