b800a15d4f5c640ec46b2b136b98e527c5ff83f7
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-05 08:59:54 -0300

    boton a form new comic, done

---
+++ frontend/src/Components/Admin/Admin.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+const Admin = () => {
+    //CRUD COMICS
+    
+    const history = useNavigate();
+    // const [comicUpdate, setComicUpdate] = React.useState({});
+    // const [deleteComic, setDeleteComic] = React.useState({});
+    // //related to business logic
+    //  const [users, setUsers] = React.useState([]);//nro de usuarios 
+    //  const [newUsers, setNewUsers] = React.useState([]);//nro de usuarios nuevos
+    //  const [lostUsers, setLostUsers] = React.useState([]);//nro de usuarios perdidos
+
+    //  const [comics, setComics] = React.useState([]);//nro de comics
+    //  const [trend, setTrend] = React.useState([]);//tendencia
+    //  const [newApiComics, setNewApiComics] = React.useState([]);//nuevos comics
+    //  const [characters, setCharacters] = React.useState([]);//personajes destacados
+     
+    //  const [earns, setEarns] = React.useState([]);//billing month
+     
+    const handleClick = () => {
+        console.log("click");
+
+    }
+
+
+
+    return (
+        <div>
+           <button onClick={() => history('/admin/addComic')}>Add New Comic</button>
+        </div>
+    )
+}
+
+export default Admin;
