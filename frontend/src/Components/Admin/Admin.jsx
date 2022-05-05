c5ff4f1752ad3ae9131d8e8c77f6a11dcb6db6d2
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-05 09:12:18 -0300

    boton a form edit comic, done

--- frontend/src/Components/Admin/Admin.jsx
+++ frontend/src/Components/Admin/Admin.jsx
@@ -1,37 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
-    //CRUD COMICS
-    
-    const history = useNavigate();
-    // const [comicUpdate, setComicUpdate] = React.useState({});
-    // const [deleteComic, setDeleteComic] = React.useState({});
-    // //related to business logic
-    //  const [users, setUsers] = React.useState([]);//nro de usuarios 
-    //  const [newUsers, setNewUsers] = React.useState([]);//nro de usuarios nuevos
-    //  const [lostUsers, setLostUsers] = React.useState([]);//nro de usuarios perdidos
+  //CRUD COMICS
 
-    //  const [comics, setComics] = React.useState([]);//nro de comics
-    //  const [trend, setTrend] = React.useState([]);//tendencia
-    //  const [newApiComics, setNewApiComics] = React.useState([]);//nuevos comics
-    //  const [characters, setCharacters] = React.useState([]);//personajes destacados
-     
-    //  const [earns, setEarns] = React.useState([]);//billing month
-     
-    const handleClick = () => {
-        console.log("click");
+  const history = useNavigate();
+  // const [comicUpdate, setComicUpdate] = React.useState({});
+  // const [deleteComic, setDeleteComic] = React.useState({});
+  // //related to business logic
+  //  const [users, setUsers] = React.useState([]);//nro de usuarios
+  //  const [newUsers, setNewUsers] = React.useState([]);//nro de usuarios nuevos
+  //  const [lostUsers, setLostUsers] = React.useState([]);//nro de usuarios perdidos
 
-    }
+  //  const [comics, setComics] = React.useState([]);//nro de comics
+  //  const [trend, setTrend] = React.useState([]);//tendencia
+  //  const [newApiComics, setNewApiComics] = React.useState([]);//nuevos comics
+  //  const [characters, setCharacters] = React.useState([]);//personajes destacados
 
+  //  const [earns, setEarns] = React.useState([]);//billing month
 
+  const handleClick = () => {
+    console.log("click");
+  };
 
-    return (
-        <div>
-           <button onClick={() => history('/admin/addComic')}>Add New Comic</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+      {/*goes to form to create */}
+        <button onClick={() => history("/admin/addComic")}>
+          Add New Comic
+        </button>
+      </div>
+       {/*goes to form to editar, agregar input para search by id */}
+      <button type="input" onClick={() => history("/admin/editComic")}>
+        Edit Comic
+      </button>
+    </div>
+  );
+};
 
 export default Admin;
