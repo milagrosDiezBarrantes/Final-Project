639bc753c703eeac08ad0903b8d0dbca82c13e37
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-05-07 19:48:38 -0300

    routes user plans

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,36 @@ conn.sync({ force: false }).then(() => {
 });
 
 
+const { Plans } = require('../backend/src/db');
+var plan1 =  Plans.findOrCreate({
+  where:{
+    name:"monthly",
+    amount:7
+},
+
+});
+
+var plan2 =  Plans.findOrCreate({
+  where:{
+    name:"annual",
+    amount:70
+  },
+
+});
+var plan3 =  Plans.findOrCreate({
+  where:{
+    name:"admin",
+    amount:0
+  },
+
+});
+
+
+let planes= Plans.findAll()
+
+Promise.all([plan1,plan2,planes,plan3])
+  .then(res => {
+    console.log([plan1,plan2]);
+  }).then(res => {
+    console.log(planes.Promise);
+  });
