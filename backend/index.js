c52331305eb0f3ccdfde1671d1fb8409dc72fb3c
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-05-17 00:02:13 -0300

    re deploy

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
-	server.listen(3001, () => {
-		console.log("%s listening at 3001");
+conn.sync({ force: true }).then(() => {
+	server.listen(process.env.PORT, () => {
+		console.log(`%s listening at ${process.env.PORT}`);
 	});
 });
 /*hola que hace */
-const { Plans, Users } = require("../backend/src/db");
+
+setTimeout(() => {
+	const { Plans, Users } = require("../backend/src/db");
+
 var superAdmin = Users.findOrCreate({
 	where: {
 		email: "soyadmin@henry.com",
@@ -52,3 +55,5 @@ Promise.all([plan1, plan2, plan3, plan4, superAdmin, planes])
 	.then((res) => {
 		console.log(planes.Promise);
 	});//54465
+
+  }, 10000);
